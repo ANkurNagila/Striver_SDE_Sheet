3a34487700a45b4e1a257dcf912cd459ed1ee010
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2022-06-07 12:15:02 +0530

    Update best_time_to_buy_sell_stock.cpp

--- DAY_1/best_time_to_buy_sell_stock.cpp
+++ DAY_1/best_time_to_buy_sell_stock.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h> 
-int maximumProfit(vector<int> &prices){
+int maximumProfit(vector<int> &arr){
+    if(arr.size()<=1)
+        return 0;
     
+    int mini=arr[0],maxi=arr[0];
+    int ans=0;
     
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]>maxi)
+            maxi=arr[i];
+        
+        ans=max(ans,maxi-mini);
+        
+        if(arr[i]<mini){
+            mini=arr[i];
+            maxi=arr[i];
+        }
+    }
     
+    ans=max(ans,maxi-mini);
+    return ans;
+   
 }
