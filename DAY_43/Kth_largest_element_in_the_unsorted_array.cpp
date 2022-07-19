76070ea6322cfca7139e10e9ba77cc789010d659
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2022-07-20 00:17:16 +0530

    Update Kth_largest_element_in_the_unsorted_array.cpp

--- DAY_43/Kth_largest_element_in_the_unsorted_array.cpp
+++ DAY_43/Kth_largest_element_in_the_unsorted_array.cpp
@@ -3,3 +3,24 @@ int kthLargest(vector<int>& arr, int size, int k)
     sort(arr.begin(),arr.end());
     return arr[size-k];
 }
+
+
+********************************************* Using Heap ***************************************
+
+#include<queue>
+int kthLargest(vector<int>& input, int n, int k)
+{
+    priority_queue<int ,vector<int> ,greater<int>> x;
+    
+    for(int i=0;i<k;i++)
+        x.push(input[i]);
+    
+    for(int i=k;i<n;i++){
+        if(x.top()<input[i]){
+            x.pop();
+            x.push(input[i]);
+        }
+    }
+    
+    return x.top();
+}
